6a5e0dfc81e61fc60728d0ebdb865d51b8d4b5f4
alex0blackwell <alex.blackwell314@gmail.com>
2020-06-13 10:50:16 -0700

    Colour matching working

--- src/App.js
+++ src/App.js
@@ -3,31 +3,92 @@ import './App.css';
 
 
 function Tile(props) {
+  let click, classname;
+  classname = "tile";
+  if(props.colour !== "#3b3b3b") {
+    click = props.onClick;
+    classname += " pointer";
+  }
+  if(props.action === 1)
+    classname += " raise";
+
   return(
-    <div className="col-lg-4 col-md-4 col-6">
-      <div className="tile" onClick={props.onClick} style={{backgroundColor: props.colour}}>
+    <div className="col-lg-2 col-md-2 col-sm-2 col-4">
+      <div className={classname} onClick={click} style={{backgroundColor: props.colour}}>
       </div>
     </div>
   );
 }
 
+
 class Votes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      oneFlipped: false,
+      roundScore: 0,
+      numFlipped: 0,
+      colourToMatch: null,
+      indexOfLast: null,
       colours: this.randomiseColours(),
+      tileAction: Array(18).fill(0),
     }
   }
 
-  handleClick(colour) {
-    console.log("the colour is", colour);
-    // let totalPresses = this.state.tileVotes;
-    // totalPresses[i]++;
-    // this.setState({
-    //   // oneFlipped: !oneFlipped,
-    //   tileVotes: totalPresses,
-    // });
+  handleClick(colour, index) {
+    this.state.numFlipped++;
+    if(this.state.numFlipped === 2) {
+      if(this.state.colourToMatch === colour && this.state.indexOfLast !== index) {
+        console.log("Nice, that's a match");
+        let newScore = this.state.roundScore++;
+        this.setState({
+          roundScore: newScore,
+        });
+        this.updateColours([index,this.state.indexOfLast], 2);
+        this.renderTiles()
+      } else {
+        console.log("Not epic, not a match");
+      }
+      let newTileAction = this.state.tileAction;
+      newTileAction[this.state.indexOfLast] = 0;
+      newTileAction[index] = 0;
+      this.setState({
+        numFlipped: 0,
+        colourToMatch: null,
+        indexOfLast: null,
+        tileAction: newTileAction,
+      });
+    }
+    else if(this.state.numFlipped === 1) {
+      console.log("waiting for next...")
+      let newTileAction = this.state.tileAction;
+      newTileAction[index] = 1;
+      // console.log(tindex, newTileAction);
+      this.setState({
+        colourToMatch: colour,
+        indexOfLast: index,
+        tileAction: newTileAction,
+      });
+    }
+
+  }
+
+  updateColours(indexChangeArr, action) {
+    // where action is 0 for normal, 1 for raised, and 2 for dead
+    let newColours = this.state.colours;
+
+    for(let i = 0; i < indexChangeArr.length; i++) {
+      if(action === 2) {
+        // if dead
+        newColours[indexChangeArr[i]] = "#3b3b3b";
+      }
+      else if(action === 1) {
+        // if raised
+      }
+    }
+
+    this.setState({
+      colours: newColours,
+    });
   }
 
   randomiseColours() {
@@ -42,7 +103,6 @@ class Votes extends React.Component {
       doubles.push(allColours[i]);
       i++;
     }
-    // console.log("res before is is: ", res);
 
     // then randomise it
     for(let i = 0; i < doubles.length; i++) {
@@ -59,17 +119,22 @@ class Votes extends React.Component {
   renderTiles() {
     let res = [];
 
+    let actionArr = this.state.tileAction;
+    // console.log(actionArr);
     const colours = this.state.colours;
 
     const listItems = colours.map((colour, index) =>
       res.push(<Tile
         key={index.toString()}
-        onClick={() => this.handleClick(colour)}
-        colour={colour}/>),
+        onClick={() => this.handleClick(colour, index)}
+        colour={colour}
+        action={actionArr[index]}
+        />),
     );
 
     return (res);
   }
+
 
   render() {
     return(
